46410229be8a5e8a46790b224ef9781ebee72335
“Milad <miladsoftware@icloud.com>
2021-06-23 15:17:00 +0430

    Initial Commit

---
+++ Shared/FoodioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodioApp.swift
+//  Shared
+//
+//  Created by Milad Golchinpour on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
